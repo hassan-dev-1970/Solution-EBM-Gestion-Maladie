b7cf6ad0bb5c537948a7b19dbfd54ffaa746f1ab
ELWADEH <elwadehhassan1970@gmail.com>
2026-02-16 14:15:08 +0000

    Fix MySQL port parsing production

--- backend-express/src/config/db.js
+++ backend-express/src/config/db.js
@@ -11,8 +11,10 @@ host: process.env.DB_HOST,
 user: process.env.DB_USER,
 password: process.env.DB_PASSWORD,
 database: process.env.DB_NAME,
-port: parseInt(process.env.DB_PORT, 10)
-
+port: parseInt(process.env.DB_PORT, 10),
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
